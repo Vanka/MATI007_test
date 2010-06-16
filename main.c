3be89a5f5d1d2ad7f6d9b0b05185383ad2e20c9e
TReaper <SF-298@yandex.ru>
2010-06-17 02:26:45 +0800

    Initisal commit

---
+++ main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LINELENGTH 33
+
+
+int main()
+{
+    char namebuffer[LINELENGTH];
+    char surnamebuffer[LINELENGTH];
+    char nicknamebuffer[LINELENGTH];
+
+    int c;
+
+    while(c!='@'){
+
+        printf("How do you want to introduce yourself?:\n");
+        printf("1)By name\n");
+        printf("2)By surname\n");
+        printf("3)By nickname\n");
+        printf("4)I'm not shy!!!\n");
+        printf("5)Tut bil Gero\n");
+        printf("6)Print something cool =)\n");
+
+        do{
+            c=getch();
+        }while(c<'1'||c>'6');
+
+        printf("Remember:you can't enter more then %d symbols per field!\n", LINELENGTH-1);
+
+        switch(c){
+            case '1':
+                printf("Enter your name:");
+                fgets(namebuffer, LINELENGTH, stdin);
+                namebuffer[strlen(namebuffer)-1]='\0';
+                break;
+            case '2':
+                printf("Enter your surname:");
+                fgets(surnamebuffer, LINELENGTH, stdin);
+                surnamebuffer[strlen(surnamebuffer)-1]='\0';
+                break;
+            case '3':
+                printf("Enter your nickname:");
+                fgets(nicknamebuffer, LINELENGTH, stdin);
+                nicknamebuffer[strlen(nicknamebuffer)-1]='\0';
+                break;
+            case '4':
+
+                printf("Enter your name:");
+                fgets(namebuffer, LINELENGTH, stdin);
+                namebuffer[strlen(namebuffer)-1]='\0';
+
+                printf("Enter your surname:");
+                fgets(surnamebuffer, LINELENGTH, stdin);
+                surnamebuffer[strlen(surnamebuffer)-1]='\0';
+
+                printf("Enter your nickname:");
+                fgets(nicknamebuffer, LINELENGTH, stdin);
+                nicknamebuffer[strlen(nicknamebuffer)-1]='\0';
+
+                break;
+            case '6':
+                printf("You've got FROWNED!1!\n");
+                getch();
+                printf("Seriously, You've got FROWNED!1!\n");
+                break;
+
+
+        }
+
+        //printf("name=%d\nsurname=%d\nnickname=%d\n", strlen(namebuffer), strlen(surnamebuffer), strlen(nicknamebuffer));
+/*
+        namebuffer[strlen(namebuffer)-1]='\0';
+        surnamebuffer[strlen(surnamebuffer)-1]='\0';
+        nicknamebuffer[strlen(nicknamebuffer)-1]='\0';
+  */
+
+
+        if(strcmp(nicknamebuffer, "TReaper")==0){
+            printf("Welcome, Master TReaper!\n");
+        }else if(c=='1'){
+            printf("So, you've entered only your name... You are %s, right?\n", namebuffer);
+        }else if(c=='2'){
+            printf("So, your surname is %s\n", surnamebuffer);
+        }else if(c=='3'){
+            printf("So, you've entered only your nickname, which is %s\n", nicknamebuffer);
+        }else if(c=='4'){
+            printf("Oh, %s is not a shy person - now we know that %s's surname is %s and %s's nickname is %s! Thank you very much!!!\n", namebuffer, namebuffer, surnamebuffer, namebuffer, nicknamebuffer);
+        }
+
+        printf("Enter @ to exit or something else to try again lol\n");
+        c=getch();
+    }
+    return 0;
+}
